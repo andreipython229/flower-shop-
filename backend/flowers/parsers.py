0067b249b9f244138087ce5e03c356c71ed8e449
Andrei Zienkovich <panbbbqv@gmail.com>
2025-12-29 17:59:51 +0300

    Добавлен маппинг изображения для карточки 131 - Розовые розы (11 шт)

--- backend/flowers/parsers.py
+++ backend/flowers/parsers.py
@@ -33,6 +33,8 @@ FLOWER_IMAGE_MAP = {
     "Белые розы (25 шт)": "https://media.istockphoto.com/id/1304715857/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%BE%D0%B9-%D0%B1%D1%83%D0%BA%D0%B5%D1%82-%D1%86%D0%B2%D0%B5%D1%82%D1%83%D1%89%D0%B8%D1%85-%D1%86%D0%B2%D0%B5%D1%82%D0%BE%D0%B2-%D0%BA%D1%80%D1%83%D0%BF%D0%BD%D1%8B%D0%BC-%D0%BF%D0%BB%D0%B0%D0%BD%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%BE%D0%B2%D0%B0%D1%8F-%D1%82%D0%B5%D0%BA%D1%81%D1%82%D1%83%D1%80%D0%B0-%D0%BF%D0%BE%D1%87%D0%BA%D0%B8-%D0%B1%D0%B5%D0%BB%D1%8B%D1%85-%D1%80%D0%BE%D0%B7-%D0%B2-%D0%B2%D0%B0%D0%B7%D0%B5-%D1%86%D0%B2%D0%B5%D1%82%D1%83%D1%89%D0%B8%D0%B5.jpg?s=612x612&w=is&k=20&c=Ak3ONsNOQUVpD1Mg7Y7khKZwTEqXwSN6E8BVxetTa1U=",
     # Карточка 132 - "Розовые розы (7 шт)"
     "Розовые розы (7 шт)": "https://thumbs.dreamstime.com/b/%D0%BA%D1%80%D1%83%D0%BF%D0%BD%D1%8B%D0%BC-%D0%BF%D0%BB%D0%B0%D0%BD%D0%BE%D0%BC-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D1%85-%D1%80%D0%BE%D0%B7%D0%BE%D0%B2%D1%8B%D1%85-%D1%80%D0%BE%D0%B7-%D0%B1%D1%83%D0%BA%D0%B5%D1%82-%D0%B4%D0%BB%D1%8F-valentine-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD-%D0%B8-%D1%81%D0%B2%D0%B0%D0%B4%D1%8C%D0%B1%D0%B0-166652135.jpg",
+    # Карточка 131 - "Розовые розы (11 шт)"
+    "Розовые розы (11 шт)": "https://i.pinimg.com/474x/11/64/26/11642647756e3ef97f8eac0457275bda.jpg",
     # Добавим остальные по мере получения скриншотов
 }
 
